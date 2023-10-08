e8923ae59d8e21f919b7c12b5a68dbeefaf7485f
saske <saskedir@mail.ru>
2023-10-08 18:13:58 +0300

    homework 2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,5 @@
+import csv  # Импортируем модуль csv для работы с CSV файлами
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +15,21 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self._name = name  # Сделаем атрибут name приватным (начинается с "_")
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) > 10:
+            self._name = new_name[:10]  # Обрезаем строку, если она больше 10 символов
+        else:
+            self._name = new_name
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +44,23 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, csv_file_path):
+        cls.all.clear()
+        with open(csv_file_path, mode='r', encoding='cp1251') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                name = row['name']
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                item = cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(string):
+        try:
+            return int(float(string))
+        except ValueError:
+            raise ValueError("Не удалось преобразовать строку в число")
+
+# Код в файле main.py остается без изменений
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,10 +1,24 @@
 from src.item import Item
+
+
 def test_calculate_total_price():
     item = Item("Смартфон", 10000, 20)
     assert item.calculate_total_price() == 200000
+
 
 def test_apply_discount():
     item = Item("Смартфон", 10000, 20)
     Item.pay_rate = 0.8
     item.apply_discount()
     assert item.price == 8000.0
+
+
+
+def test_name_lenght():
+    '''Длина имени не более 10'''
+    item = Item('test1', 20.0, 5)
+    item.name = 'This is long name'
+    assert item.name == 'This is lo'
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
