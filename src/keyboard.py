64fc8dd2ad9f7484bdd8633cb2889fd05cb4b57f
saske <saskedir@mail.ru>
2023-10-22 16:55:21 +0300

    fix bag

--- src/keyboard.py
+++ src/keyboard.py
@@ -19,23 +19,4 @@ class LanguageMixin:
 class Keyboard(Item, LanguageMixin):
     def __init__(self, name, price, quantity):
         super().__init__(name, price, quantity)
-        LanguageMixin.__init__(self)
         self._name = name
-
-    def __repr__(self):
-        class_name = self.__class__.__name__
-        return f"{class_name}('{self._name}', {self.price}, {self.quantity})"
-
-    def __str__(self):
-        return self._name
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, new_name):
-        if len(new_name) > 10:
-            self._name = new_name[:10]
-        else:
-            self._name = new_name
